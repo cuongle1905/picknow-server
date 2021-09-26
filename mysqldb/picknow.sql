eeae01f03775af412f44f8e67176d4250168b706
cuongle1905 <cuongle1905@gmail.com>
2021-09-26 07:27:17 +0700

    update database - add products

--- mysqldb/picknow.sql
+++ mysqldb/picknow.sql
@@ -16,6 +16,56 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `brand` (
+  `id` int NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `parent` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_brand_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+INSERT INTO `brand` VALUES (1,'CoopMart',NULL),(2,'Coop Select',1),(3,'Orion',NULL),(4,'ChocoPie',3),(5,'Richeese',NULL),(6,'Nabati',5),(7,'Danisa',NULL),(8,'Gouté',3),(9,'Tường An',NULL),(10,'Nam Ngư',NULL),(11,'Liên Thành',NULL),(12,'Ba Huân',NULL),(13,'Unilever',NULL),(14,'Omo',13),(15,'P & G',NULL),(16,'Ariel',15),(17,'Lifebuoy',13);
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `color`
+--
+
+DROP TABLE IF EXISTS `color`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `color` (
+  `id` smallint NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_color_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `color`
+--
+
+LOCK TABLES `color` WRITE;
+/*!40000 ALTER TABLE `color` DISABLE KEYS */;
+/*!40000 ALTER TABLE `color` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `company`
 --
 
@@ -24,7 +74,7 @@ DROP TABLE IF EXISTS `company`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company` (
   `id` int NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `phone` varchar(30) DEFAULT NULL,
   `phone2` varchar(30) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -70,7 +120,7 @@ DROP TABLE IF EXISTS `country`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `country` (
   `id` smallint NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -95,7 +145,7 @@ DROP TABLE IF EXISTS `district`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `district` (
   `id` int NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `province` smallint NOT NULL,
   `code` smallint DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -172,6 +222,99 @@ INSERT INTO `image_info` VALUES (1,'46cc79c5-1d73-11ec-b980-00ffc71f7bf7.png'),(
 UNLOCK TABLES;
 
 --
+-- Table structure for table `origin`
+--
+
+DROP TABLE IF EXISTS `origin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `origin` (
+  `id` smallint NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_origin_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `origin`
+--
+
+LOCK TABLES `origin` WRITE;
+/*!40000 ALTER TABLE `origin` DISABLE KEYS */;
+INSERT INTO `origin` VALUES (1,'Việt Nam');
+/*!40000 ALTER TABLE `origin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` bigint NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `unit` int NOT NULL,
+  `code` varchar(45) DEFAULT NULL,
+  `brand` int DEFAULT NULL,
+  `origin` smallint DEFAULT '1',
+  `size` int DEFAULT NULL,
+  `color` smallint DEFAULT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_product_name` (`name`),
+  KEY `idx_product_code` (`code`),
+  KEY `fk_product_unit_idx` (`unit`),
+  KEY `fk_product_brand_idx` (`brand`),
+  KEY `fk_product_size_idx` (`size`),
+  KEY `fk_product_color_idx` (`color`),
+  KEY `fk_product_origin_idx` (`origin`),
+  CONSTRAINT `fk_product_brand` FOREIGN KEY (`brand`) REFERENCES `brand` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_color` FOREIGN KEY (`color`) REFERENCES `color` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_origin` FOREIGN KEY (`origin`) REFERENCES `origin` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_size` FOREIGN KEY (`size`) REFERENCES `size` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_unit` FOREIGN KEY (`unit`) REFERENCES `unit` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Bột yến mạch nếp cẩm Coop Select 450g',23,NULL,2,1,NULL,NULL,NULL),(2,'Yến mạch nguyên hạt Coop Select 400g',24,NULL,2,1,NULL,NULL,NULL),(3,'Bánh Chocopie Orion hộp 396g',11,NULL,4,1,NULL,NULL,NULL),(4,'Bánh xốp Nabati Richeese hộp 20×7,5g',11,NULL,6,1,NULL,NULL,NULL),(5,'Bánh Chocopie Orion Cacao hộp 360g',11,NULL,4,1,NULL,NULL,NULL),(6,'Bánh Quy Bơ Danisa hộp thiếc 200g',11,NULL,7,1,NULL,NULL,NULL),(7,'Bánh mè giòn Goute Orion hộp 288g',11,NULL,8,1,NULL,NULL,NULL),(8,'Mực rim me Coop Select 100g',28,NULL,2,1,NULL,NULL,NULL),(9,'Dầu ăn Tường An Cooking Nutri Plus chai 1L',25,NULL,9,1,NULL,NULL,NULL),(10,'Dầu ăn Tường An chai 1L',25,NULL,9,1,NULL,NULL,NULL),(11,'Nước mắm Nam Ngư 3in1 chai 750ml',19,NULL,10,1,NULL,NULL,NULL),(12,'Nước mắm Nam Ngư 3in1 chai 900ml',20,NULL,10,1,NULL,NULL,NULL),(13,'Nước mắm cao cấp Liên Thành nhãn bạc chai thủy tinh 40 độ đạm 600ml',21,NULL,11,1,NULL,NULL,NULL),(14,'Nước mắm Liên Thành nhãn đồng 25 độ đạm chai thủy tinh 600ml',21,NULL,11,1,NULL,NULL,NULL),(15,'Trứng Gà Ba Huân vỉ 10 trứng',22,NULL,12,1,NULL,NULL,NULL),(16,'Trứng vịt Ba Huân vỉ 10 trứng',22,NULL,12,1,NULL,NULL,NULL),(17,'Cá rô làm sạch',2,NULL,NULL,1,NULL,NULL,NULL),(18,'Cá lóc cắt lát gói 300g',30,NULL,NULL,1,NULL,NULL,NULL),(19,'Nước giặt OMO Matic Comfort Tinh dầu thơm túi 3.7kg',31,NULL,14,1,NULL,NULL,NULL),(20,'Nước giặt Ariel Matic cửa trước tươi mát túi 1.85kg',32,NULL,16,1,NULL,NULL,NULL),(21,'Nước rửa tay Lifebuoy bảo vệ vượt trội diệt khuẩn chai 500g',33,NULL,17,1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_attribute`
+--
+
+DROP TABLE IF EXISTS `product_attribute`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_attribute` (
+  `id` int NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_product_attribute_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_attribute`
+--
+
+LOCK TABLES `product_attribute` WRITE;
+/*!40000 ALTER TABLE `product_attribute` DISABLE KEYS */;
+INSERT INTO `product_attribute` VALUES (1,'Brand'),(4,'Color'),(2,'Origin'),(5,'Packing Type'),(3,'Size');
+/*!40000 ALTER TABLE `product_attribute` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `province`
 --
 
@@ -180,7 +323,7 @@ DROP TABLE IF EXISTS `province`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `province` (
   `id` smallint NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `country` smallint NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
@@ -200,6 +343,30 @@ INSERT INTO `province` VALUES (1,'TP. HCM',1);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `size`
+--
+
+DROP TABLE IF EXISTS `size`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `size` (
+  `id` int NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_size_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `size`
+--
+
+LOCK TABLES `size` WRITE;
+/*!40000 ALTER TABLE `size` DISABLE KEYS */;
+/*!40000 ALTER TABLE `size` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `store`
 --
 
@@ -208,24 +375,24 @@ DROP TABLE IF EXISTS `store`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `store` (
   `id` int NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `province` smallint NOT NULL,
   `district` int NOT NULL,
   `ward` int NOT NULL,
   `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `company` int DEFAULT NULL,
-  `contact_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `store_group` int NOT NULL,
+  `contact_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `contact_phone` varchar(20) DEFAULT NULL,
   `contact_phone2` varchar(20) DEFAULT NULL,
   `website` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_company_idx` (`company`),
   KEY `fk_ward_idx` (`ward`),
   KEY `fk_province_idx` (`province`),
   KEY `fk_district_idx` (`district`),
-  CONSTRAINT `fk_store_company` FOREIGN KEY (`company`) REFERENCES `company` (`id`) ON UPDATE CASCADE,
+  KEY `fk_store_store_group_idx` (`store_group`),
   CONSTRAINT `fk_store_district` FOREIGN KEY (`district`) REFERENCES `district` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_store_province` FOREIGN KEY (`province`) REFERENCES `province` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_store_store_group` FOREIGN KEY (`store_group`) REFERENCES `store_group` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_store_ward` FOREIGN KEY (`ward`) REFERENCES `ward` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -236,8 +403,87 @@ CREATE TABLE `store` (
 
 LOCK TABLES `store` WRITE;
 /*!40000 ALTER TABLE `store` DISABLE KEYS */;
-INSERT INTO `store` VALUES (1,'CoopMart NDC',1,3,27,'NDC',2,NULL,NULL,NULL,NULL),(2,'CoopSmile 01 NDC',1,1,1,'ABC',5,NULL,NULL,NULL,NULL),(3,'FamilyMart Nguyễn Bỉnh Khiêm',1,1,2,'3 Nguyễn Bỉnh Khiêm',2,NULL,NULL,NULL,NULL),(4,'FamilyMart 20 Nguyễn Đình Chiểu',1,1,2,'20 Nguyễn Đình Chiểu',2,NULL,NULL,NULL,NULL),(5,'CoopMart Hiệp Bình Chánh',1,3,28,'1248/4 Quốc Lộ 23',1,NULL,NULL,NULL,NULL),(6,'FamilyMart 22 Xô Viết Nghệ Tĩnh',1,6,75,'22 Xô Viết Nghệ Tĩnh',2,NULL,NULL,NULL,NULL);
+INSERT INTO `store` VALUES (1,'CoopMart NDC',1,3,27,'NDC',1,NULL,NULL,NULL,NULL),(2,'CoopSmile 01 NDC',1,1,1,'ABC',2,NULL,NULL,NULL,NULL),(3,'FamilyMart Nguyễn Bỉnh Khiêm',1,1,2,'3 Nguyễn Bỉnh Khiêm',3,NULL,NULL,NULL,NULL),(4,'FamilyMart 20 Nguyễn Đình Chiểu',1,1,2,'20 Nguyễn Đình Chiểu',3,NULL,NULL,NULL,NULL),(5,'CoopMart Hiệp Bình Chánh',1,3,28,'1248/4 Quốc Lộ 23',1,NULL,NULL,NULL,NULL),(6,'FamilyMart 22 Xô Viết Nghệ Tĩnh',1,6,75,'22 Xô Viết Nghệ Tĩnh',3,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store_group`
+--
+
+DROP TABLE IF EXISTS `store_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store_group` (
+  `id` int NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_store_group_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store_group`
+--
+
+LOCK TABLES `store_group` WRITE;
+/*!40000 ALTER TABLE `store_group` DISABLE KEYS */;
+INSERT INTO `store_group` VALUES (4,'CircleK'),(1,'CoopMart'),(2,'CoopSmile'),(3,'FamiliyMart');
+/*!40000 ALTER TABLE `store_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store_product`
+--
+
+DROP TABLE IF EXISTS `store_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store_product` (
+  `store_product_group` int NOT NULL,
+  `product` bigint NOT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`store_product_group`,`product`),
+  KEY `fk_store_product_product_idx` (`product`),
+  CONSTRAINT `fk_store_product_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_store_product_store_product_group` FOREIGN KEY (`store_product_group`) REFERENCES `store_product_group` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store_product`
+--
+
+LOCK TABLES `store_product` WRITE;
+/*!40000 ALTER TABLE `store_product` DISABLE KEYS */;
+INSERT INTO `store_product` VALUES (41,15,28000),(42,3,50500),(42,4,24000),(42,5,50500),(42,6,55300),(42,7,52400),(42,8,40500),(43,9,46000),(43,10,38500),(44,11,44700),(44,12,53000),(44,13,71200),(44,14,62300),(45,16,31000),(46,17,75900),(46,18,52000),(47,19,199000),(47,20,112000),(48,21,69000);
+/*!40000 ALTER TABLE `store_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store_product_group`
+--
+
+DROP TABLE IF EXISTS `store_product_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store_product_group` (
+  `id` int NOT NULL,
+  `store_group` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `parent` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store_product_group`
+--
+
+LOCK TABLES `store_product_group` WRITE;
+/*!40000 ALTER TABLE `store_product_group` DISABLE KEYS */;
+INSERT INTO `store_product_group` VALUES (1,1,'Công nghệ',NULL),(2,1,'Đồ dùng',NULL),(3,1,'Hóa mỹ phẩm',NULL),(4,1,'May mặc',NULL),(5,1,'Tươi sống',NULL),(6,1,'Bánh kẹo',1),(7,1,'Dầu ăn - nước chấm',1),(8,1,'Đông lạnh',1),(9,1,'Gia vị - hạt nêm',1),(10,1,'Mì - bún - cháo - nui',1),(11,1,'Nước uống - rượu bia',1),(12,1,'Sản phẩm đóng gói',1),(13,1,'Sữa - bột',1),(14,1,'Dụng cụ',2),(15,1,'Điện',2),(16,1,'Gia đình',2),(17,1,'Nhà bếp',2),(18,1,'Nhựa',2),(19,1,'Bột giặt - nước xả',3),(20,1,'Chất tẩy rửa',3),(21,1,'Dầu gội -  sữa tắm',3),(22,1,'Đồ em bé',3),(23,1,'Giấy - khăn',3),(24,1,'Mỹ phẩm',3),(25,1,'Vệ sinh cá nhân',3),(26,1,'Đồng phục học sinh',4),(27,1,'Khẩu trang',4),(28,1,'Thời trang nam',4),(29,1,'Thời trang nữ',4),(30,1,'Tổng hợp',4),(31,1,'Bánh mì',5),(32,1,'Gia cầm',5),(33,1,'Hải sản',5),(34,1,'Hoa tươi',5),(35,1,'Rau - củ - quả',5),(36,1,'Thịt - Cá',5),(37,1,'Thực phẩm đóng gói',5),(38,1,'Thực phẩm nấu chín',5),(39,1,'Thực phẩm sơ chế',5),(40,1,'Trái cây',5),(41,1,'Trứng vịt - gà - cút',5),(42,1,'Bánh - snack',6),(43,1,'Dầu ăn - Dầu hào',7),(44,1,'Nước mắm',7),(45,1,'Trứng vịt',41),(46,1,'Cá',36),(47,1,'Nước Giặt',19),(48,1,'Nước - Gel Rửa Tay',25);
+/*!40000 ALTER TABLE `store_product_group` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -248,7 +494,7 @@ DROP TABLE IF EXISTS `string_object`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `string_object` (
-  `id` varchar(1000) DEFAULT NULL
+  `id` varchar(1000) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -262,6 +508,36 @@ LOCK TABLES `string_object` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `unit`
+--
+
+DROP TABLE IF EXISTS `unit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `unit` (
+  `id` int NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `short_unit` int DEFAULT NULL,
+  `base_unit` tinyint DEFAULT '1',
+  `exchange` double DEFAULT '1',
+  `weight` double DEFAULT NULL,
+  `volumn` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_unit_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unit`
+--
+
+LOCK TABLES `unit` WRITE;
+/*!40000 ALTER TABLE `unit` DISABLE KEYS */;
+INSERT INTO `unit` VALUES (1,'each',NULL,1,1,NULL,NULL),(2,'kg',NULL,2,1,NULL,NULL),(3,'L',NULL,3,1,NULL,NULL),(4,'m',NULL,4,1,NULL,NULL),(7,'chai',NULL,1,1,NULL,NULL),(8,'gói',NULL,1,1,NULL,NULL),(10,'cái',NULL,1,1,NULL,NULL),(11,'hộp',NULL,1,1,NULL,NULL),(12,'vỉ',NULL,1,1,NULL,NULL),(13,'lốc',NULL,1,1,NULL,NULL),(14,'lọ',NULL,1,1,NULL,NULL),(15,'cuộn',NULL,1,1,NULL,NULL),(16,'cây',NULL,1,1,NULL,NULL),(17,'bịch',NULL,1,1,NULL,NULL),(18,'túi',NULL,1,1,NULL,NULL),(19,'chai 750ml',7,1,1,NULL,0.75),(20,'chai 900ml',7,1,1,NULL,0.9),(21,'chai 600ml',7,1,1,NULL,0.6),(22,'vỉ 10 trứng',12,1,1,NULL,NULL),(23,'gói 450g',8,1,1,0.45,NULL),(24,'gói 400g',8,1,1,0.4,NULL),(25,'chai 1L',7,1,1,NULL,1),(26,'bộ',NULL,1,1,NULL,NULL),(27,'hũ',NULL,1,1,NULL,NULL),(28,'hũ 100g',27,1,1,0.1,NULL),(29,'cuốn',NULL,1,1,NULL,NULL),(30,'gói 300g',8,1,1,0.3,NULL),(31,'túi 3.7kg',18,1,1,3.7,NULL),(32,'túi 1.85kg',18,1,1,1.85,NULL),(33,'chai 500g',7,1,1,0.5,NULL);
+/*!40000 ALTER TABLE `unit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ward`
 --
 
@@ -270,7 +546,7 @@ DROP TABLE IF EXISTS `ward`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ward` (
   `id` int NOT NULL,
-  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `district` int NOT NULL,
   `code` int DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -316,4 +592,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-25  5:40:29
+-- Dump completed on 2021-09-26  7:26:36
